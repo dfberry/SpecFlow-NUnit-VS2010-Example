163503987e88fbcb5d8076afc34a53de1dcced46
Dina Berry <dina@berryintl.com>
2012-03-27 15:18:52 -0700

    first commit

---
+++ Calculator.spec/StepDefinition1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace Calculator.spec
+{
+    [Binding]
+    public class StepDefinition1
+    {
+        Calculator calc = new Calculator();
+
+        [Given("I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredSomethingIntoTheCalculator(int result)
+        {
+            //TODO: implement arrange (recondition) logic
+            // For storing and retrieving scenario-specific data, 
+            // the instance fields of the class or the
+            //     ScenarioContext.Current
+            // collection can be used.
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            calc.Add(result);
+        }
+
+        [When("I press add")]
+        public void WhenIPressAdd()
+        {
+            //TODO: implement act (action) logic
+
+            calc.Sum();
+        }
+
+        [Then("the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            //TODO: implement assert (verification) logic
+
+            Assert.AreEqual(result,calc.Total());
+        }
+    }
+}
